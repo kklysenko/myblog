65f6ab477d1da8b910f58e9a1ce93641cf2b022f
IamBrave <gameron27@gmail.com>
2020-04-26 21:24:36 +0300

    added main tags and functions

--- wp-content/themes/myblog/inc/template-functions.php
+++ wp-content/themes/myblog/inc/template-functions.php
@@ -11,6 +11,24 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
+
+const POST_PER_PAGE = 5;
+
+function start_session() {
+    if( ! session_id() ) {
+        session_start();
+    }
+}
+add_action('init', 'start_session', 1);
+
+function end_session() {
+    session_destroy ();
+}
+
+add_action('wp_logout','end_session');
+add_action('wp_login','end_session');
+add_action('end_session_action','end_session');
+
 function myblog_body_classes( $classes ) {
 	// Adds a class of hfeed to non-singular pages.
 	if ( ! is_singular() ) {
@@ -35,3 +53,146 @@ function myblog_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'myblog_pingback_header' );
+
+/**
+ * Add ACF theme option setting.
+ */
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page();
+
+}
+
+/**
+ * Excerpt's max length.
+ */
+function my_excerpt_length( $length ) {
+	return 24;
+}
+add_filter( 'excerpt_length', 'my_excerpt_length' );
+
+/**
+ * Edit excerpt format.
+ */
+function new_excerpt_more( $more ) {
+	return '...';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+if ( ! function_exists( 'update_post_likes' ) ) :
+    /**
+     * Loadmore posts action.
+     */
+    function more_post_ajax() {
+
+        $ppp = (isset($_POST["postsPerPage"])) ? $_POST["postsPerPage"] : POST_PER_PAGE;
+        $current_page = (isset($_POST["currentPage"])) ? $_POST["currentPage"] : 1;
+        $max_pages = (isset($_POST["maxPages"])) ? $_POST["maxPages"] : 1;
+
+        header("Content-Type: text/html");
+
+        $offset = $ppp * $current_page;
+
+        $args = array(
+            'post_type'      => 'post',
+            'posts_per_page' => $ppp,
+            'paged'          => $current_page,
+            'offset'         => $offset,
+        );
+
+        $query = new WP_Query($args);
+        $posts_count = \count( $query->posts );
+
+        $out = array(
+            'posts'        => '',
+            'posts_amount' => $posts_count,
+            'pagination'   => '',
+        );
+
+        if ($query -> have_posts()) :  while ($query -> have_posts()) : $query -> the_post();
+            $id = get_the_ID();
+            $icon_class = array_search( $id, $_SESSION['likes'] ) ? 'like-icon_active' : '';
+            $out['posts'] .= '<div class="post-block__post">
+                <a href="' . get_the_permalink() .'" class="post-block__post-img">
+                    <img src="' . get_the_post_thumbnail_url($post->ID, 'full') . '" alt="image">
+                </a>
+                <div class="post-block__post__content">
+                    <div class="like" data-post-id="' . $id . '">
+                        <div class="like-row">
+                            <i class="like-icon ' . $icon_class . '"></i>
+                        </div>
+                        <div class="like-row">
+                            <div class="like-title">Likes:
+                            <span class="like-value"> ' . get_post_likes( $id ) . '</span>
+                        </div>
+                        </div>
+                    </div>
+                    <a href="' . get_the_permalink() . '">
+                        <h3>' . get_the_title() . '</h3>
+                    </a>
+                    <div class="fszM lh15">' . get_the_excerpt() . '</div>
+                    <div class="post-block__post__content-likes">
+                    </div>
+                </div>
+            </div>';
+
+            endwhile;
+            $out['status'] = 'success';
+        else :
+            $out['status'] = 'unsuccess';
+        endif;
+
+        wp_reset_postdata();
+        die( json_encode( $out ) );
+    }
+endif;
+
+add_action('wp_ajax_nopriv_more_post_ajax', 'more_post_ajax');
+add_action('wp_ajax_more_post_ajax', 'more_post_ajax');
+
+if ( ! function_exists( 'update_post_likes' ) ) :
+    /**
+     * Update post likes value.
+     *
+    */
+    function update_post_likes()
+    {
+        $id = (isset($_POST["id"])) ? $_POST["id"] : null;
+        $action = (isset($_POST["whatDo"])) ? $_POST["whatDo"] : 'add';
+
+        $post_meta = get_post_meta( $id );
+
+        $userLikes = $_SESSION['likes'];
+        $postLikes = $post_meta[ LIKES_AMOUNT ] ? $post_meta[ LIKES_AMOUNT ][0] : 0;
+
+        $likes = $action == 'add' ? $postLikes+1 : $postLikes-1;
+
+        $res = [];
+        $res['likes_amount'] = $likes;
+
+        if
+        (
+            ( ! in_array( $id, $userLikes ) && $action == 'add')
+            || ( in_array( $id, $userLikes ) && $action != 'add')
+            )
+        {
+            if ( $action == 'add' ) {
+                $_SESSION['likes'][] = $id;
+            } else {
+                $index = array_search( $id, $_SESSION['likes'] );
+                $_SESSION['likes'][$index] = null;
+            }
+
+            $res['status'] = update_post_meta( $id, LIKES_AMOUNT, $likes );
+        } else {
+            $res['status'] = 'error';
+        }
+
+        die( json_encode( $res ) );
+
+    }
+
+endif;
+
+add_action('wp_ajax_nopriv_update_post_likes', 'update_post_likes');
+add_action('wp_ajax_update_post_likes', 'update_post_likes');
--- wp-content/themes/myblog/inc/template-tags.php
+++ wp-content/themes/myblog/inc/template-tags.php
@@ -7,6 +7,10 @@
  * @package myblog
  */
 
+const BLOG_PAGE_SLUG = 'page_for_posts';
+const LIKES_USER     = 'likes_users';
+const LIKES_AMOUNT   = 'likes_amount';
+
 if ( ! function_exists( 'myblog_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time.
@@ -56,110 +60,217 @@ if ( ! function_exists( 'myblog_entry_footer' ) ) :
 	/**
 	 * Prints HTML with meta information for the categories, tags and comments.
 	 */
-	function myblog_entry_footer() {
-		// Hide category and tag text for pages.
-		if ( 'post' === get_post_type() ) {
-			/* translators: used between list items, there is a space after the comma */
-			$categories_list = get_the_category_list( esc_html__( ', ', 'myblog' ) );
-			if ( $categories_list ) {
-				/* translators: 1: list of categories. */
-				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'myblog' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
+    function myblog_entry_footer() {
+        // Hide category and tag text for pages.
+        if ( 'post' === get_post_type() ) {
+            /* translators: used between list items, there is a space after the comma */
+            $categories_list = get_the_category_list( esc_html__( ', ', 'myblog' ) );
+            if ( $categories_list ) {
+                /* translators: 1: list of categories. */
+                printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'myblog' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+            }
 
-			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'myblog' ) );
-			if ( $tags_list ) {
-				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'myblog' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-			}
-		}
+            /* translators: used between list items, there is a space after the comma */
+            $tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'myblog' ) );
+            if ( $tags_list ) {
+                /* translators: 1: list of tags. */
+                printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'myblog' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+            }
+        }
 
-		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-			echo '<span class="comments-link">';
-			comments_popup_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: post title */
-						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'myblog' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				)
-			);
-			echo '</span>';
-		}
+        if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+            echo '<span class="comments-link">';
+            comments_popup_link(
+                sprintf(
+                    wp_kses(
+                        /* translators: %s: post title */
+                        __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'myblog' ),
+                        array(
+                            'span' => array(
+                                'class' => array(),
+                            ),
+                        )
+                    ),
+                    wp_kses_post( get_the_title() )
+                )
+            );
+            echo '</span>';
+        }
 
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'myblog' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
-	}
+        edit_post_link(
+            sprintf(
+                wp_kses(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                    __( 'Edit <span class="screen-reader-text">%s</span>', 'myblog' ),
+                    array(
+                        'span' => array(
+                            'class' => array(),
+                        ),
+                    )
+                ),
+                wp_kses_post( get_the_title() )
+            ),
+            '<span class="edit-link">',
+            '</span>'
+        );
+    }
 endif;
 
 if ( ! function_exists( 'myblog_post_thumbnail' ) ) :
-	/**
-	 * Displays an optional post thumbnail.
-	 *
-	 * Wraps the post thumbnail in an anchor element on index views, or a div
-	 * element when on single views.
-	 */
-	function myblog_post_thumbnail() {
-		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
-			return;
-		}
+    /**
+     * Displays an optional post thumbnail.
+     *
+     * Wraps the post thumbnail in an anchor element on index views, or a div
+     * element when on single views.
+     */
+    function myblog_post_thumbnail() {
+        if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+            return;
+        }
 
-		if ( is_singular() ) :
-			?>
+        if ( is_singular() ) :
+            ?>
 
-			<div class="post-thumbnail">
-				<?php the_post_thumbnail(); ?>
-			</div><!-- .post-thumbnail -->
+            <div class="post-thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div><!-- .post-thumbnail -->
 
-		<?php else : ?>
+        <?php else : ?>
 
-			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-				<?php
-					the_post_thumbnail(
-						'post-thumbnail',
-						array(
-							'alt' => the_title_attribute(
-								array(
-									'echo' => false,
-								)
-							),
-						)
-					);
-				?>
-			</a>
+            <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+                <?php
+                    the_post_thumbnail(
+                        'post-thumbnail',
+                        array(
+                            'alt' => the_title_attribute(
+                                array(
+                                    'echo' => false,
+                                )
+                            ),
+                        )
+                    );
+                ?>
+            </a>
 
-			<?php
-		endif; // End is_singular().
-	}
+            <?php
+        endif; // End is_singular().
+    }
 endif;
 
 if ( ! function_exists( 'wp_body_open' ) ) :
-	/**
-	 * Shim for sites older than 5.2.
-	 *
-	 * @link https://core.trac.wordpress.org/ticket/12563
-	 */
-	function wp_body_open() {
-		do_action( 'wp_body_open' );
-	}
+    /**
+     * Shim for sites older than 5.2.
+     *
+     * @link https://core.trac.wordpress.org/ticket/12563
+     */
+    function wp_body_open() {
+        do_action( 'wp_body_open' );
+    }
 endif;
+
+if ( ! function_exists( 'the_acf_option' ) ) :
+    /**
+     * Displays an ACF option field
+     *
+    */
+    function the_acf_option( $label )
+    {
+        if ( function_exists( 'the_field' ) ) :
+            the_field( $label, 'option' );
+        else :
+            echo 'Please, activate ACF plugin.';
+        endif;
+    }
+endif;
+
+if ( ! function_exists( 'the_acf_blog' ) ) :
+    /**
+     * Displays an ACF blog field
+     *
+    */
+    function the_acf_blog( $label )
+    {
+        if ( function_exists( 'the_field' ) ) :
+            the_field( $label, get_option( BLOG_PAGE_SLUG ) );
+        else :
+            echo 'Please, activate ACF plugin.';
+        endif;
+    }
+endif;
+
+if ( ! function_exists( 'the_acf_blog_repeater' ) ) :
+    /**
+     * Displays an ACF repeater field
+     *
+    */
+    function the_acf_blog_repeater( $label )
+    {
+        if ( function_exists( 'the_field' ) ) :
+            the_repeater_field( $label, get_option( BLOG_PAGE_SLUG ) );
+        else :
+            echo 'Please, activate ACF plugin.';
+        endif;
+    }
+endif;
+
+if ( ! function_exists( 'get_post_likes' ) ) :
+    /**
+     * Returns post likes value.
+     *
+     * @param int $id
+     *
+    */
+    function get_post_likes( int $id )
+    {
+        $res = get_post_meta( $id, 'likes_amount' );
+
+        return $res == [] ? 0 : $res[0];
+    }
+
+endif;
+
+function popular_post()
+{
+    ob_start();
+    $query = new WP_Query( array(
+        'orderby' => 'post_views_count',
+        'posts_per_page' => 3
+        ) );
+    ?>
+        <?php
+        while ( $query->have_posts() ) {
+            $query->the_post(); ?>
+
+            <div class="sidebar-item__post">
+                <a href="<?php the_permalink(); ?>" class="sidebar-item__post-img">
+                    <img src="<?php the_post_thumbnail_url('full'); ?>" alt="image">
+                </a>
+                <div class="sidebar-item__post-content">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    <div class="sidebar-item__post-content__likes">
+                    </div>
+                </div>
+            </div>
+        <?php
+        } ?>
+    <?php
+
+    $string = ob_get_contents();
+    ob_clean();
+    wp_reset_postdata();
+
+    return $string;
+}
+
+add_shortcode('popularpost' , 'popular_post' );
+
+function wpb_tags()
+{
+	$wpbtags =  get_tags();
+	$string = '<ul class="sidebar-item__tags">';
+	foreach ($wpbtags as $tag) {
+		$string .= '<li><a href="'. get_tag_link($tag->term_id) .'" >'. $tag->name . '</a></li>' . "\n"   ;
+	}
+	$string .= '</ul>';
+	return $string;
+}
