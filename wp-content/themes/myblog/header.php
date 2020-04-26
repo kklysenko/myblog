3e88b5a9df666b5407ca09dc5749400a5258c8ab
IamBrave <gameron27@gmail.com>
2020-04-26 21:31:02 +0300

    edited header

--- wp-content/themes/myblog/header.php
+++ wp-content/themes/myblog/header.php
@@ -23,37 +23,8 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'myblog' ); ?></a>
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$myblog_description = get_bloginfo( 'description', 'display' );
-			if ( $myblog_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $myblog_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'myblog' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+    <header id="masthead" class="header">
+        <div class="container">
+            <a href="<?php echo get_site_url(); ?>"><?php esc_html_e( 'My blog', 'myblog' ); ?></a>
+        </div><!-- #container -->
+    </header><!-- #masthead -->
