f7976d221ac5d9c5e2f375022ae8ad8e63972864
IamBrave <gameron27@gmail.com>
2020-04-26 21:23:39 +0300

    prepared theme

--- wp-content/themes/myblog/footer.php
+++ wp-content/themes/myblog/footer.php
@@ -12,19 +12,6 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'myblog' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'myblog' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'myblog' ), 'myblog', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
--- wp-content/themes/myblog/functions.php
+++ wp-content/themes/myblog/functions.php
@@ -50,9 +50,9 @@ if ( ! function_exists( 'myblog_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'myblog' ),
+                'top_menu' => esc_html__( 'Top menu', 'myblog' ),
 			)
-		);
+        );
 
 		/*
 		 * Switch default core markup for search form, comment form, and comments
@@ -143,16 +143,22 @@ add_action( 'widgets_init', 'myblog_widgets_init' );
  * Enqueue scripts and styles.
  */
 function myblog_scripts() {
-	wp_enqueue_style( 'myblog-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'myblog-style', 'rtl', 'replace' );
+    global $wp_query;
 
-	wp_enqueue_script( 'myblog-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_style( 'myblog-style', get_stylesheet_uri(), array(), _S_VERSION );
+    wp_enqueue_style( 'myblog-common-style', get_template_directory_uri() . '/assets/css/common.css', array(), _S_VERSION );
+    wp_enqueue_style( 'myblog-main-style', get_template_directory_uri() . '/assets/css/main.css', array(), _S_VERSION );
+    wp_enqueue_style( 'myblog-blog-style', get_template_directory_uri() . '/assets/css/blog.css', array(), _S_VERSION );
+    wp_style_add_data( 'myblog-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'myblog-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'myblog-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), _S_VERSION, true );
+    wp_enqueue_script( 'myblog-common', get_template_directory_uri() . '/assets/js/common.js', array('jquery'), _S_VERSION, true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    wp_localize_script( 'myblog-common', 'ajax_posts', array(
+        'ajaxurl' => admin_url( 'admin-ajax.php' ),
+        'noposts' => __('No older posts found', 'myblog'),
+        'posts_per_page' => $wp_query->query_vars['posts_per_page'],
+    ));
 }
 add_action( 'wp_enqueue_scripts', 'myblog_scripts' );
 
